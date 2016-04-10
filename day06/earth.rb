8a554d672ece2d5331d59bd6fa4cc8ae3a03609e
muzhi <muzhi00@gmail.com>
2016-04-11 02:30:26 +0800

    custom singleton

--- day06/earth.rb
+++ day06/earth.rb
@@ -1,15 +1,35 @@
 require "test/unit"
 require "singleton"
 
-class Earch
+class Earth
   include Singleton
+end
+
+class Earth2
+  private_class_method :new, :clone, :dup
+
+  def self.instance
+    @singleton ||= new
+  end
 end
 
 class EarthTest < Test::Unit::TestCase
 
   def test_singleton
-    earth = Earch.instance
-    earth2 = Earch.instance
+    common_test(Earth)
+  end
+
+  def test_custom_singleton
+    common_test(Earth2)
+  end
+
+  def common_test(clazz)
+    assert_raise(NoMethodError) { clazz.new }
+    assert_raise(NoMethodError) { clazz.new.dup }
+    assert_raise(NoMethodError) { clazz.new.clone }
+
+    earth = clazz.instance
+    earth2 = clazz.instance
     assert earth.equal?(earth2)
     assert earth.object_id == earth2.object_id
   end
