46b6d6c7b49b6e886f797e6fe96480441c36ca34
muzhi <muzhi00@gmail.com>
2016-04-15 15:34:29 +0800

    eval

---
+++ day19/eval.rb
@@ -0,0 +1,51 @@
+require "minitest/autorun"
+
+class Eval
+
+  def initialize
+    @x = "I am in scope"
+  end
+
+  def i_am_a_method
+    @x = "I am in a " + __method__.to_s.tr("_", " ")
+    binding
+  end
+
+  def eval_in_method
+    eval("@x")
+  end
+
+  def eval_in_method2
+    eval("@x", i_am_a_method)
+  end
+
+  def instance_eval_in_method
+    instance_eval { @x }
+  end
+
+  def instance_eval_in_method2
+    instance_eval do
+      def in_stack? ; false; end
+    end
+  end
+end
+
+class EvalTest < MiniTest::Unit::TestCase
+
+  def setup
+    @e = Eval.new
+  end
+
+  def test_eval
+    assert_equal "I am in scope", @e.eval_in_method
+    assert_equal "I am in a " + "i_am_a_method".to_s.tr("_", " "), @e.eval_in_method2
+  end
+
+  def test_instance_eval
+    assert_equal "I am in scope", @e.instance_eval_in_method
+    assert_equal :in_stack?, @e.instance_eval_in_method2
+    assert_includes @e.singleton_methods(false), :in_stack?
+  end
+end
+
+
