f35a095975a844e6dd7736d8aaf158818667633e
muzhi <muzhi00@gmail.com>
2016-05-01 00:12:39 +0800

    change password

--- day26/init.lua
+++ day26/init.lua
@@ -3,7 +3,7 @@ gpio.mode(1, gpio.INPUT)
 flag = gpio.read(1)
 tmr.delay(5000)
 if gpio.read(1) == 0 and flag == 0 then
-    file.remove("connected")
+  file.remove("connected")
 end
 if file.open("connected","r") == nil then
   dofile("connect.lua")
