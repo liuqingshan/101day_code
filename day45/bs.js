864cef3d95f4ec294accaf65bd36d60b063a97a4
liuqingshan <liuqingshan@yeah.net>
2016-05-31 21:34:43 +0800

    fix bug

--- day45/bs.js
+++ day45/bs.js
@@ -1,6 +1,6 @@
 function shiftedDiff(first,second){
   for(var i=0; i< first.length; i++) {
-    for(var j=1; j<= i) {
+    for(var j=1; j<= i; j++) {
       r = seconde.indexOf(first.substring(i, j))
       if(r === -1) {
          return r;
